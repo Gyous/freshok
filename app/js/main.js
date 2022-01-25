90ce3b75f44d36ae974511b600bf05c7b41a1a63
Gyous <subrice26462@gmail.com>
2022-01-25 23:55:43 +0300

    12 homework

---
+++ app/js/main.js
@@ -0,0 +1,24 @@
+$(function () {
+  $('.catalog__btn').on('click', function () {
+    $('.catalog__list').toggleClass('catalog__list--active');
+    $('.catalog__btn').toggleClass('catalog__btn--active');
+  });
+
+  $(document).mouseup(function (e) {
+    let btn = $('.catalog__btn');
+    if (!btn.is(e.target) && btn.has(e.target).length === 0) {
+      $('.catalog__list').removeClass('catalog__list--active');
+      $('.catalog__btn').removeClass('catalog__btn--active');
+    }
+  });
+
+  $('.main-slider__items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    fade: true,
+    prevArrow: '<button class="slick-prev"><svg class="slick-arrow__svg slick-prev--svg"><use href="images/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button class="slick-next"><svg class="slick-arrow__svg slick-next--svg"><use href="images/sprite.svg#arrow-right"></use></svg></button>'
+  });
+  let mixer = mixitup('.products__items');
+});
